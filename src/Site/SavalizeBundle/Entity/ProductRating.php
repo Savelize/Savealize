e3b3b04102456911a2b0a91c33310f07a9e8bcd8
Samar <samar@ubuntu.(none)>
2013-06-08 18:39:35 +0200

    commit 1

---
+++ src/Site/SavalizeBundle/Entity/ProductRating.php
@@ -0,0 +1,164 @@
+<?php
+
+namespace Site\SavalizeBundle\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * ProductRating
+ *
+ * @ORM\Table(name="product_rating")
+ * @ORM\Entity
+ */
+class ProductRating
+{
+    /**
+     * @var integer
+     *
+     * @ORM\Column(name="id", type="integer", nullable=false)
+     * @ORM\Id
+     * @ORM\GeneratedValue(strategy="IDENTITY")
+     */
+    private $id;
+
+    /**
+     * @var integer
+     *
+     * @ORM\Column(name="rating", type="integer", nullable=false)
+     */
+    private $rating;
+
+    /**
+     * @var \UserAccount
+     *
+     * @ORM\ManyToOne(targetEntity="UserAccount")
+     * @ORM\JoinColumns({
+     *   @ORM\JoinColumn(name="user_id", referencedColumnName="id")
+     * })
+     */
+    private $user;
+
+    /**
+     * @var \Brand
+     *
+     * @ORM\ManyToOne(targetEntity="Brand")
+     * @ORM\JoinColumns({
+     *   @ORM\JoinColumn(name="brand_id", referencedColumnName="id")
+     * })
+     */
+    private $brand;
+
+    /**
+     * @var \Product
+     *
+     * @ORM\ManyToOne(targetEntity="Product")
+     * @ORM\JoinColumns({
+     *   @ORM\JoinColumn(name="product_id", referencedColumnName="id")
+     * })
+     */
+    private $product;
+
+
+
+    /**
+     * Get id
+     *
+     * @return integer 
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * Set rating
+     *
+     * @param integer $rating
+     * @return ProductRating
+     */
+    public function setRating($rating)
+    {
+        $this->rating = $rating;
+    
+        return $this;
+    }
+
+    /**
+     * Get rating
+     *
+     * @return integer 
+     */
+    public function getRating()
+    {
+        return $this->rating;
+    }
+
+    /**
+     * Set user
+     *
+     * @param \Site\SavalizeBundle\Entity\UserAccount $user
+     * @return ProductRating
+     */
+    public function setUser(\Site\SavalizeBundle\Entity\UserAccount $user = null)
+    {
+        $this->user = $user;
+    
+        return $this;
+    }
+
+    /**
+     * Get user
+     *
+     * @return \Site\SavalizeBundle\Entity\UserAccount 
+     */
+    public function getUser()
+    {
+        return $this->user;
+    }
+
+    /**
+     * Set brand
+     *
+     * @param \Site\SavalizeBundle\Entity\Brand $brand
+     * @return ProductRating
+     */
+    public function setBrand(\Site\SavalizeBundle\Entity\Brand $brand = null)
+    {
+        $this->brand = $brand;
+    
+        return $this;
+    }
+
+    /**
+     * Get brand
+     *
+     * @return \Site\SavalizeBundle\Entity\Brand 
+     */
+    public function getBrand()
+    {
+        return $this->brand;
+    }
+
+    /**
+     * Set product
+     *
+     * @param \Site\SavalizeBundle\Entity\Product $product
+     * @return ProductRating
+     */
+    public function setProduct(\Site\SavalizeBundle\Entity\Product $product = null)
+    {
+        $this->product = $product;
+    
+        return $this;
+    }
+
+    /**
+     * Get product
+     *
+     * @return \Site\SavalizeBundle\Entity\Product 
+     */
+    public function getProduct()
+    {
+        return $this->product;
+    }
+}
