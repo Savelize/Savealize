d1a447030e03cf700551546fedb9f196aae9e4d6
samar <samar@ubuntu>
2013-06-12 09:21:00 +0200

    new actions in the default controller but not totally finished

--- src/Site/SavalizeBundle/Controller/DefaultController.php
+++ src/Site/SavalizeBundle/Controller/DefaultController.php
@@ -10,8 +10,14 @@ class DefaultController extends Controller
     {
         return $this->render('SiteSavalizeBundle:Default:index.html.twig');
     }
-    public function showReviewsAction()
+    public function showOneReviewAction()
     {
-    	return $this->render('SiteSavalizeBundle:Default:reviews.html.twig', array());
+    	return $this->render('SiteSavalizeBundle:Default:oneReview.html.twig', array());
+    }
+    public function showAllReviews()
+    {
+    	 $em=$this->getDoctrine()->getEntityManager();
+    	 $categories = $em->getRepository('SiteSavalizeBundle:Category')->findAll();
+    	 return $this->render('SiteSavalizeBundle:Default:allReviews.html.twig', array('cats' => $categories));
     }
 }
--- src/Site/SavalizeBundle/SiteSavalizeBundle.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-namespace Site\SavalizeBundle;
-
-use Symfony\Component\HttpKernel\Bundle\Bundle;
-
-class SiteSavalizeBundle extends Bundle
-{
-}
